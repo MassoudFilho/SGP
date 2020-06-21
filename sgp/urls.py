048de157dd66f20da40dbe4f5685ef284ebb2ff6
Massoud Filho <massoud.filho@gmail.com>
2020-06-21 16:57:59 -0300

    Melhorando documentação sobre django.contrib.auth.urls

--- sgp/urls.py
+++ sgp/urls.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
+"""
+django.contrib.auth.urls
+
+Inserção do conjunto de urls account. Conjunto que envolve ações com a conta do usuário.
+Duas urls importantes para nós:
+    account/login [name=’login’]: URL que terá nosso form de login;
+    account/logout[name=’login’]: URL que fará o logout do usuário;
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('sgp/', include('saude.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # Inserção do conjunto de urls account, conjunto que envolve ações com a conta do usuário.
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
@@ -19,3 +27,5 @@ admin.site.site_header = 'Secretaria de Gestão de Pessoas do TJPA'
 admin.site.site_title = 'SGP'
 # Título da página índice.
 admin.site.index_title = 'Sistema de Gerenciamento'
+
+
