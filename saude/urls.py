ea453ee8cb5f5ca172f9106e9fdfad5cb9c0f026
Massoud Filho <massoud.filho@gmail.com>
2020-07-11 18:38:39 -0300

    URL para a página inicial da SGP

--- saude/urls.py
+++ saude/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from saude.views import home_saude, atestado, producao, mostraproducao
-
+from core.views import usuario_logado
 
 urlpatterns = [
+    path('', usuario_logado, name='sgp'),
     path('saude/', home_saude, name='home_saude'),
     path('saude/atestado/', atestado, name='atestado'),
     path('saude/producao/', producao, name='producao'),
